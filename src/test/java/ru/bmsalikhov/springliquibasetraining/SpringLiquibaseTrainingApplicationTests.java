7092ae5b1abc7f6c6b983b143d30408a9c06624e
Bulat Salikhov <bmsalikhov@gmail.com>
2024-03-29 16:45:39 +0300

    - добавил 2 миграции (1 на xml, 2 на yml)

---
+++ src/test/java/ru/bmsalikhov/springliquibasetraining/SpringLiquibaseTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmsalikhov.springliquibasetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
